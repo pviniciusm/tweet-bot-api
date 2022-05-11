cc637d0a0fe5ba09e84dcc5a8ec3dd08efb1671e
JAMERSON FELIPE PEREIRA PAZ <86246952+Ljames666@users.noreply.github.com>
2022-05-11 17:30:23 -0300

    cria class setQuoted

--- src/features/tweet/domain/usecases/set-tweet-usecase.ts
+++ src/features/tweet/domain/usecases/set-tweet-usecase.ts
@@ -1,14 +1,27 @@
 import { InvalidParamsError } from '../../../../core/domain/errors/invalid-params-error';
+import { Tweet } from '../../../../core/infra/database/entities/tweet';
 import { TwitterService } from '../../../../core/infra/services/twitter-service';
 
-export class SetSingleTweet {
-  public repository: TwitterService;
+type quoteTweetData = {
+  id: string;
+  text: string;
+};
 
+export class SetSingleTweet {
   async run(text: string): Promise<Object> {
     if (!text) {
       throw new InvalidParamsError();
     }
-    const NewTweet = this.repository.postTweet(text);
+    const NewTweet = TwitterService.postTweet(text);
+    return NewTweet;
+  }
+}
+export class SetQuotedTweet {
+  async run(data: quoteTweetData): Promise<any> {
+    if (!data) {
+      throw new InvalidParamsError();
+    }
+    const NewTweet = TwitterService.tweetQuote(data.id, data.text);
     return NewTweet;
   }
 }
