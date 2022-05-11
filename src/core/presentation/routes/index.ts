88ce9963a0369e980f42d69318c26fa7752e9562
JAMERSON FELIPE PEREIRA PAZ <86246952+Ljames666@users.noreply.github.com>
2022-05-11 18:11:16 -0300

    add tweet routes

--- src/core/presentation/routes/index.ts
+++ src/core/presentation/routes/index.ts
@@ -1,5 +1,7 @@
-import { UserRoutes } from "../../../features/user/presentation/routes/user-routes";
+import { TweetRoutes } from '../../../features/tweet/presentation/routes/tweetRoutes';
+import { UserRoutes } from '../../../features/user/presentation/routes/user-routes';
 
 export const makeRoutes = (app: any) => {
-    app.use("/user", UserRoutes.getRoutes());
+  app.use('/user', UserRoutes.getRoutes());
+  app.use('/tweet', TweetRoutes.getRoutes());
 };
