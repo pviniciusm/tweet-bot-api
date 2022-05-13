c81dcc90d142630e2391bec0a75d6a78ef7c24e3
SamuelMazuim <samuelmazuim@hotmail.com>
2022-05-12 21:14:11 -0300

    Co-authored-by: Paulo Vinicius Cardoso <pvinics@gmail.com> Co-authored-by: JAMERSON FELIPE PEREIRA PAZ <Ljames666@users.noreply.github.com> Co-authored-by: Matheus Petry Pfitscher <matheusppfitscher@outlook.com>

--- src/core/presentation/server/index.ts
+++ src/core/presentation/server/index.ts
@@ -3,15 +3,23 @@ import express from "express";
 import cors from "cors";
 
 export class Server {
+    private static app: any;
+
     static initialize() {
+        this.app = this.getServer();
+
+        this.app.listen(8081, () => {
+            console.log("Server is running.");
+        });
+    }
+
+    static getServer() {
         const app = express();
         app.use(express.json());
         app.use(cors());
 
         makeRoutes(app);
 
-        app.listen(8081, () => {
-            console.log("Server is running.");
-        });
+        return app;
     }
 }
